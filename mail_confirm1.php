b69ff253c981a49d7b7e5c50edf65c8c5d5693e3
asatokouji <asatokouji0326@gmail.com>
2024-01-24 12:47:08 +0900

    Lesson_contactform

---
+++ mail_confirm1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>お問い合わせファームを作る</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+ <h1>ああああ</h1>
+</body>
+</html>
+
